6bae57d2b1029c8004789ab157cab1095e7b63e6
Luis Gomez de Terreros Oramas <gomezyoramas@gmail.com>
2025-12-08 19:33:32 +0100

    fix: Linter error

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,10 +1,14 @@
 import random
-from locust import HttpUser, TaskSet, task, events
-from core.environment.host import get_host_for_locust_testing
-from core.locust.common import get_csrf_token
+
+from locust import HttpUser, TaskSet, events, task
+
 from app import create_app
 from app.modules.dataset import services
+from core.environment.host import get_host_for_locust_testing
+from core.locust.common import get_csrf_token
+
 dataset_ids = []
+
 
 @events.test_start.add_listener
 def on_test_start(environment, **kwargs):
@@ -14,10 +18,8 @@ def on_test_start(environment, **kwargs):
         datasets = services.DataSetService().latest_synchronized()
         dataset_ids = [ds.id for ds in datasets]
         print(f"Loaded {len(dataset_ids)} dataset IDs from database")
-            
-            
-                
-   
+
+
 class DatasetBehavior(TaskSet):
     def on_start(self):
         self.dataset()
@@ -32,15 +34,16 @@ class DatasetBehavior(TaskSet):
         if not dataset_ids:
             print("No dataset IDs available")
             return
-            
+
         url_name = "/dataset/[id]/badge.json"
         dataset_id = random.choice(dataset_ids)
-        
+
         with self.client.get(f"/dataset/{dataset_id}/badge.json", name=url_name, catch_response=True) as response:
             if response.status_code != 200:
                 response.failure(f"Failed for ID {dataset_id}: {response.status_code}")
             else:
                 response.success()
+
 
 class DatasetUser(HttpUser):
     tasks = [DatasetBehavior]
