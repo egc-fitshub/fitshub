2d6f50340ecf629c0da70b52f536af0315ebc73d
AlvaroBarragan <trabajoalvaro1909@gmail.com>
2025-12-10 18:30:39 +0100

    test: test communities

--- app/modules/community/tests/test_selenium.py
+++ app/modules/community/tests/test_selenium.py
@@ -1,4 +1,14 @@
 import time
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from selenium.common.exceptions import NoSuchElementException
 
@@ -15,12 +25,27 @@ def test_community_index():
         # Open the index page
         driver.get(f"{host}/community")
 
+
         # Wait a little while to make sure the page has loaded completely
         time.sleep(4)
 
         try:
-            pass
-
+            driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(1)").click()
+            driver.find_element(By.ID, "email").click()
+            driver.find_element(By.ID, "email").send_keys("admin@example.com")
+            driver.find_element(By.ID, "password").click()
+            driver.find_element(By.ID, "password").send_keys("1234")
+            driver.find_element(By.ID, "submit").click()
+            driver.find_element(By.LINK_TEXT, "My communities").click()
+            driver.find_element(By.LINK_TEXT, "Test community 2").click()
+            driver.find_element(By.LINK_TEXT, "Edit Community Details").click()
+            driver.find_element(By.ID, "description").click()
+            driver.find_element(By.ID, "description").send_keys("This is a test description for selenium")
+            driver.find_element(By.ID, "submit").click()
+            driver.find_element(By.LINK_TEXT, "Create a community").click()
+            driver.find_element(By.CSS_SELECTOR, ".active .align-middle:nth-child(2)").click()
+            driver.find_element(By.CSS_SELECTOR, ".sidebar-item:nth-child(2) .align-middle:nth-child(2)").click()
+            driver.find_element(By.CSS_SELECTOR, ".content").click()
         except NoSuchElementException:
             raise AssertionError("Test failed!")
 
@@ -31,3 +56,8 @@ def test_community_index():
 
 # Call the test function
 test_community_index()
+
+    
+ 
+   
+  
